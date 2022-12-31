2043dd962608f8ff7479f9c289ed41b09a39cf2f
Jan Pikman <pikmanjan@gmail.com>
2022-12-31 22:35:00 +0100

    Documentation for evaluate

--- src/evaluate.jl
+++ src/evaluate.jl
@@ -1,7 +1,31 @@
 export evaluate
 
+"""
+    evaluate(type :: Symbol, θ :: Real, x :: Real)
+
+Returns boolean corresponding to condition `x <= θ`.
+Function is used mainly during learning because it is not dependent on any 
+structure.
+"""
 evaluate(type :: Symbol, θ :: Real, x :: Real) = x <= θ
+
+"""
+    evaluate(type :: Symbol, θ :: Nothing, x :: Bool)
+
+Returns boolean corresponding to condition `x`.
+Function is used mainly during learning because it is not dependent on any 
+structure.
+"""
 evaluate(type :: Symbol, θ :: Nothing, x :: Bool) = x
+
+"""
+    evaluate(type :: Symbol, θ :: AbstractString, x :: AbstractString)
+
+Returns boolean corresponding to condition `x == θ` for symbol `:stringequality`
+and to condition `x <= θ` for symbol `:stringinequality`
+Function is used mainly during learning because it is not dependent on any 
+structure.
+"""
 function evaluate(type :: Symbol, θ :: AbstractString, x :: AbstractString) 
     if type == :stringequality
         return x == θ
