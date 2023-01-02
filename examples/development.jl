81266e839b22b82c676c7d48cd5c9e003b9ce5ca
Jan Pikman <pikmanjan@gmail.com>
2023-01-02 21:22:42 +0100

    Removing old files

--- examples/development.jl
+++ examples/development.jl
@@ -3,22 +3,17 @@ using DecisionTrees
 using Statistics
 
 # Small example
-X = [
-    -1.5  "a"
-    -1.14 "b"
-    -0.45 "aa"
-    2.5   "aaa"
-    27.4  "aaaa"
-]
+X = [-1.5  "a"; -1.14 "b"; -0.45 "aa"; 2.5 "aaa"; 27.4 "aaaa"]
 Y = [0, 1, 0, 1, 1]
 
 dt = DecisionTree()
-learn!(dt, X, Y; depth=2, attribute_count=1)
+learn!(dt, X, Y)
 dt
 Y_ = evaluate(dt, X)
-mean(Y .== Y_)
+mean(Y .!= Y_)
 
-rf = RandomForest(10)
-learn!(rf, X, Y; bagging=true)
+rf = RandomForest(100)
+learn!(rf, X, Y; depth=4, attribute_count=2, bagging=true)
+rf
 Y_ = evaluate(rf, X)
 mean(Y .== Y_)
